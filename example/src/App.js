d9b9b90df2c08a5fe0f802cfb0fff9c8ad5aa826
aldoetobex <aldoetobex@gmail.com>
2021-08-19 12:22:41 +0700

    Update

--- example/src/App.js
+++ example/src/App.js
@@ -193,6 +193,33 @@ function App() {
     return series
   }
 
+  /*
+  // this function will generate output in this format
+  // every array in data is of the format [x, y, z] where x (timestamp) and y are the two axes coordinates,
+  // z is the third coordinate, which you can interpret as the size of the bubble formed too.
+  // data = [
+     [timestamp, 23, 10],
+     [timestamp, 33, 11],
+     [timestamp, 12, 8]
+      ...
+  ]
+  */
+  function generateDataBubble(baseval, count, yrange) {
+    var i = 0
+    var series = []
+    while (i < count) {
+      var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+      var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15
+
+      series.push([x, y, z])
+      baseval += 86400000
+      i++
+    }
+    return series
+  }
+
   return (
     <div style={{ padding: '0px 20px' }}>
       <h1>react-tcomponent - telescoope.org</h1>
@@ -959,7 +986,7 @@ function App() {
           }
         ]}
         title={{
-          text: 'CandleStick Chart',
+          text: 'Chart Candle Stick',
           align: 'left'
         }}
         xaxis={{
@@ -1153,13 +1180,345 @@ function App() {
         separator=' - '
         optionColor='warna'
       />
-      <ChartRadar />
-      <ChartRange />
-      <ChartBubble />
-      <ChartPolarArea />
-      <ChartRadial />
-      <ChartScatter />
-      <ChartBoxPlot />
+      <ChartRadar
+        name='keuntungan_kerugian_radar'
+        options={[
+          {
+            warna: '#00ff00',
+            nama: 'Revenue',
+            jumlah: [10, 41, 35, 51, 49, 62]
+          },
+          {
+            warna: '#ff0000',
+            nama: 'Cost',
+            jumlah: [10, 20, 60, 3, 62, 14]
+          }
+        ]}
+        title={{
+          text: 'Chart Radar'
+        }}
+        subtitle={{
+          text: 'Keuntungan dan Kerugian'
+        }}
+        optionValue={{ nama: 'name', jumlah: 'data' }}
+        labels={['STR', 'AGI', 'VIT', 'INT', 'DEX', 'LUK']}
+        separator=' - '
+        optionColor='warna'
+      />
+      <ChartRange
+        name='saham_range'
+        subtitle={{
+          text: 'Pergerakan Saham Telescoope'
+        }}
+        options={[
+          {
+            data: [
+              {
+                x: 'TEAM A',
+                y: [65, 96]
+              },
+              {
+                x: 'TEAM B',
+                y: [55, 78]
+              },
+              {
+                x: 'TEAM C',
+                y: [95, 186]
+              }
+            ]
+          }
+        ]}
+        title={{
+          text: 'Chart Range'
+        }}
+        optionValue={{ data: 'data' }}
+        separator=' - '
+      />
+      <ChartBubble
+        options={[
+          {
+            name: 'Bubble1',
+            data: generateDataBubble(
+              new Date('11 Feb 2017 GMT').getTime(),
+              20,
+              {
+                min: 10,
+                max: 60
+              }
+            )
+          },
+          {
+            name: 'Bubble2',
+            data: generateDataBubble(
+              new Date('11 Feb 2017 GMT').getTime(),
+              20,
+              {
+                min: 10,
+                max: 60
+              }
+            )
+          },
+          {
+            name: 'Bubble3',
+            data: generateDataBubble(
+              new Date('11 Feb 2017 GMT').getTime(),
+              20,
+              {
+                min: 10,
+                max: 60
+              }
+            )
+          },
+          {
+            name: 'Bubble4',
+            data: generateDataBubble(
+              new Date('11 Feb 2017 GMT').getTime(),
+              20,
+              {
+                min: 10,
+                max: 60
+              }
+            )
+          }
+        ]}
+        title={{
+          text: 'Chart Bubble'
+        }}
+        subtitle={{
+          text: 'Peminat bulet bulet'
+        }}
+        optionValue={{ data: 'data', name: 'name' }}
+        separator=' - '
+        height={350}
+        dataLabels={{
+          enabled: false
+        }}
+        fill={{
+          opacity: 0.8
+        }}
+        xaxis={{
+          tickAmount: 12,
+          type: 'category'
+        }}
+        yaxis={{
+          max: 70
+        }}
+      />
+      <ChartPolarArea
+        options={[
+          { warna: '#7638ff', nama: 'Pizza', jumlah: 60 },
+          { warna: '#ff737b', nama: 'Ketoprak', jumlah: 20 },
+          { warna: '#fda600', nama: 'Nasi Goreng', jumlah: 30 },
+          { warna: '#ccc', nama: 'Batu', jumlah: 10 }
+        ]}
+        title={{
+          text: 'Chart Polar Area'
+        }}
+        subtitle={{
+          text: 'Peminat berdasarkan makanan'
+        }}
+        optionValue='jumlah'
+        optionLabel='nama'
+        separator=' - '
+        optionColor='warna'
+      />
+      <ChartRadial
+        options={[
+          { warna: '#7638ff', nama: 'Pizza', jumlah: 60 },
+          { warna: '#ff737b', nama: 'Ketoprak', jumlah: 20 },
+          { warna: '#fda600', nama: 'Nasi Goreng', jumlah: 30 },
+          { warna: '#ccc', nama: 'Batu', jumlah: 10 }
+        ]}
+        title={{
+          text: 'Chart Radial'
+        }}
+        subtitle={{
+          text: 'Peminat berdasarkan makanan'
+        }}
+        optionValue='jumlah'
+        optionLabel='nama'
+        separator=' - '
+        optionColor='warna'
+      />
+      <ChartScatter
+        options={[
+          {
+            name: 'SAMPLE A',
+            data: [
+              [16.4, 5.4],
+              [21.7, 2],
+              [25.4, 3],
+              [19, 2],
+              [10.9, 1],
+              [13.6, 3.2],
+              [10.9, 7.4],
+              [10.9, 0],
+              [10.9, 8.2],
+              [16.4, 0],
+              [16.4, 1.8],
+              [13.6, 0.3],
+              [13.6, 0],
+              [29.9, 0],
+              [27.1, 2.3],
+              [16.4, 0],
+              [13.6, 3.7],
+              [10.9, 5.2],
+              [16.4, 6.5],
+              [10.9, 0],
+              [24.5, 7.1],
+              [10.9, 0],
+              [8.1, 4.7],
+              [19, 0],
+              [21.7, 1.8],
+              [27.1, 0],
+              [24.5, 0],
+              [27.1, 0],
+              [29.9, 1.5],
+              [27.1, 0.8],
+              [22.1, 2]
+            ]
+          },
+          {
+            name: 'SAMPLE B',
+            data: [
+              [36.4, 13.4],
+              [1.7, 11],
+              [5.4, 8],
+              [9, 17],
+              [1.9, 4],
+              [3.6, 12.2],
+              [1.9, 14.4],
+              [1.9, 9],
+              [1.9, 13.2],
+              [1.4, 7],
+              [6.4, 8.8],
+              [3.6, 4.3],
+              [1.6, 10],
+              [9.9, 2],
+              [7.1, 15],
+              [1.4, 0],
+              [3.6, 13.7],
+              [1.9, 15.2],
+              [6.4, 16.5],
+              [0.9, 10],
+              [4.5, 17.1],
+              [10.9, 10],
+              [0.1, 14.7],
+              [9, 10],
+              [12.7, 11.8],
+              [2.1, 10],
+              [2.5, 10],
+              [27.1, 10],
+              [2.9, 11.5],
+              [7.1, 10.8],
+              [2.1, 12]
+            ]
+          },
+          {
+            name: 'SAMPLE C',
+            data: [
+              [21.7, 3],
+              [23.6, 3.5],
+              [24.6, 3],
+              [29.9, 3],
+              [21.7, 20],
+              [23, 2],
+              [10.9, 3],
+              [28, 4],
+              [27.1, 0.3],
+              [16.4, 4],
+              [13.6, 0],
+              [19, 5],
+              [22.4, 3],
+              [24.5, 3],
+              [32.6, 3],
+              [27.1, 4],
+              [29.6, 6],
+              [31.6, 8],
+              [21.6, 5],
+              [20.9, 4],
+              [22.4, 0],
+              [32.6, 10.3],
+              [29.7, 20.8],
+              [24.5, 0.8],
+              [21.4, 0],
+              [21.7, 6.9],
+              [28.6, 7.7],
+              [15.4, 0],
+              [18.1, 0],
+              [33.4, 0],
+              [16.4, 0]
+            ]
+          }
+        ]}
+        title={{
+          text: 'Chart Scatter'
+        }}
+        subtitle={{
+          text: 'Peminat bulet bulet'
+        }}
+        optionValue={{ data: 'data', name: 'name' }}
+        separator=' - '
+        dataLabels={{
+          enabled: false
+        }}
+        fill={{
+          opacity: 0.8
+        }}
+        xaxis={{
+          tickAmount: 12,
+          type: 'category'
+        }}
+        yaxis={{
+          max: 70
+        }}
+      />
+      <ChartBoxPlot
+        name='saham_range'
+        subtitle={{
+          text: 'Pergerakan Saham Telescoope'
+        }}
+        options={[
+          {
+            type: 'boxPlot',
+            data: [
+              {
+                x: 'Jan 2015',
+                y: [54, 66, 69, 75, 88]
+              },
+              {
+                x: 'Jan 2016',
+                y: [43, 65, 69, 76, 81]
+              },
+              {
+                x: 'Jan 2017',
+                y: [31, 39, 45, 51, 59]
+              },
+              {
+                x: 'Jan 2018',
+                y: [39, 46, 55, 65, 71]
+              },
+              {
+                x: 'Jan 2019',
+                y: [29, 31, 35, 39, 44]
+              },
+              {
+                x: 'Jan 2020',
+                y: [41, 49, 58, 61, 67]
+              },
+              {
+                x: 'Jan 2021',
+                y: [54, 59, 66, 71, 88]
+              }
+            ]
+          }
+        ]}
+        title={{
+          text: 'Chart Box Plot'
+        }}
+        optionValue={{ data: 'data', type: 'type' }}
+        separator=' - '
+      />
     </div>
   )
 }
